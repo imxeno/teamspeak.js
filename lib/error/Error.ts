d6e6b0d41794398c1e5b7bc4b00d96b41c0b4fc0
Piotr Adamczyk <xeno@xeno.yt>
2018-10-10 11:16:07 +0200

    removed empty TSJSError constructor

--- lib/error/Error.ts
+++ lib/error/Error.ts
@@ -2,28 +2,21 @@
  * @abstract
  */
 export default class TSJSError {
-    /**
-     * Constructs an instance of TSJSError
-     * @constructor
-     * @param {TSJSResponse} response response of error-type
-     */
-    constructor() {
-    }
-    /**
-     * Gets a unique pseudo error code.
-     * @abstract
-     * @returns {string} pseudo error code (error code prefixed with
-     *                   an identificator of the component which errored)
-     */
-    public getCode() {
-        throw new Error("Unimplemented");
-    }
-    /**
-     * Gets a user-friendly message describing the error.
-     * @abstract
-     * @returns {string} user-friendly message describing the error
-     */
-    public getMessage() {
-        throw new Error("Unimplemented");
-    }
+  /**
+   * Gets a unique pseudo error code.
+   * @abstract
+   * @returns {string} pseudo error code (error code prefixed with
+   *                   an identificator of the component which errored)
+   */
+  public getCode() {
+    throw new Error("Unimplemented");
   }
+  /**
+   * Gets a user-friendly message describing the error.
+   * @abstract
+   * @returns {string} user-friendly message describing the error
+   */
+  public getMessage() {
+    throw new Error("Unimplemented");
+  }
+}
