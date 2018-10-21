cf60790ce0a7f91a3302e1142797b7885633918c
Piotr Adamczyk <xeno@xeno.yt>
2018-10-21 15:30:15 +0200

    some typedefs

--- lib/util/Types.ts
+++ lib/util/Types.ts
@@ -1,7 +1,10 @@
 import TSJSConnectionError from "../error/ConnectionError";
 
-export type VirtualServerPort = number;
+export type Port = number;
+export type VirtualServerPort = Port;
 export type VirtualServerId = number;
+export type ServerGroupId = number;
+export type ChannelGroupId = number;
 export interface ServerQueryOptions {
   host: string;
   port: VirtualServerPort;
