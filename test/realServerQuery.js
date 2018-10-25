a6ff0b675fc9da04704e7065009979ea0f29182e
Piotr Adamczyk <xeno@xeno.yt>
2018-10-25 14:03:28 +0200

    moved ts3server related things to JS file, instead of travis

--- test/realServerQuery.js
+++ test/realServerQuery.js
@@ -1,27 +1,39 @@
 "use strict";
 const expect = require("chai").expect;
 const TSJSServerQuery = require("../dist/index.js").TSJSServerQuery;
+const realServer = require("./util/realServer");
 
+const serverPath = "./temp";
 const credentials = {
   client_login_name: "serveradmin",
   client_login_password: "p4ssw0rd"
 };
 
-describe("TSJSServerQuery constructor", () => {
+describe("TSJSServerQuery", () => {
+  before(async function() {
+    this.timeout(60000);
+    await realServer.startServer(
+      serverPath,
+      process.env.PLATFORM,
+      process.env.VERSION,
+      credentials.client_login_password
+    );
+  });
+
   it("should throw when trying to override an unknown option", () => {
     expect(() => {
       new TSJSServerQuery({ anUnknownOption: "nobody cares" });
     }).to.throw("Unknown option");
   });
 
-  it("should correctly override host option and preserve default port", () => {
+  it("should override host option and preserve default port", () => {
     expect(new TSJSServerQuery({ host: "example.com" }).options).to.include({
       host: "example.com",
       port: 10011
     });
   });
 
-  it("should correctly connect to a local server and login using predefined details", async () => {
+  it("should connect to a local server and login using predefined details", async () => {
     const ts = new TSJSServerQuery();
     try {
       await ts.connect();
@@ -35,7 +47,7 @@ describe("TSJSServerQuery constructor", () => {
     await ts.quit();
   });
 
-  it("should be able to logout", async () => {
+  it("should logout", async () => {
     const ts = new TSJSServerQuery();
     try {
       await ts.connect();
@@ -50,7 +62,7 @@ describe("TSJSServerQuery constructor", () => {
     await ts.quit();
   });
 
-  it("should be able to get server list", async () => {
+  it("should get server list", async () => {
     const ts = new TSJSServerQuery();
     try {
       await ts.connect();
@@ -65,7 +77,7 @@ describe("TSJSServerQuery constructor", () => {
     await ts.quit();
   });
 
-  it("should be able to get a server by id", async () => {
+  it("should get a server by id", async () => {
     const ts = new TSJSServerQuery();
     try {
       await ts.connect();
@@ -80,7 +92,7 @@ describe("TSJSServerQuery constructor", () => {
     await ts.quit();
   });
 
-  it("should be able to get a server by default port", async () => {
+  it("should get a server by default port", async () => {
     const ts = new TSJSServerQuery();
     try {
       await ts.connect();
@@ -95,7 +107,7 @@ describe("TSJSServerQuery constructor", () => {
     await ts.quit();
   });
 
-  it("should be able to restart the server", async () => {
+  it("should restart the server", async () => {
     const ts = new TSJSServerQuery();
     try {
       await ts.connect();
@@ -110,5 +122,24 @@ describe("TSJSServerQuery constructor", () => {
       throw err;
     }
     await ts.quit();
+  });
+
+  it("should stop the server", async () => {
+    const ts = new TSJSServerQuery();
+    try {
+      await ts.connect();
+      await ts.login(
+        credentials.client_login_name,
+        credentials.client_login_password
+      );
+      await ts.stopProcess();
+    } catch (err) {
+      throw err;
+    }
+    await ts.quit();
+  });
+
+  after(() => {
+    realServer.stopServer(serverPath);
   });
 });
---
+++ test/util/realServer.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+const path = require("path");
+const request = require("request-promise");
+const shelljs = require("shelljs");
+const os = require("os");
+
+const getPlatform = () => {
+  const platform = os.platform();
+  if (platform === "darwin") return "mac";
+  if (platform === "win32") {
+    if (arch === "x64") return "win64";
+    return "win32";
+  }
+  const arch = os.arch() === "x64" ? "amd64" : "x86";
+  return platform + "_" + arch;
+};
+
+module.exports = {
+  startServer: async (
+    serverPath = "./temp",
+    platform = getPlatform(),
+    version = "3.4.0",
+    serveradmin_password
+  ) => {
+    let zip = false;
+    if (platform === "mac" || platform === "win32" || platform === "win64") {
+      zip = true;
+    }
+    shelljs.exec(
+      "wget http://dl.4players.de/ts/releases/" +
+        version +
+        "/teamspeak3-server_" +
+        platform +
+        "-" +
+        version +
+        "." +
+        (zip ? "zip" : "tar.bz2") +
+        " -O " +
+        "teamspeak3-server.temp",
+      { silent: true }
+    );
+
+    if (zip) shelljs.exec("unzip teamspeak3-server.temp", { silent: true });
+    else shelljs.exec("tar xjf teamspeak3-server.temp", { silent: true });
+
+    fs.unlinkSync("teamspeak3-server.temp");
+    shelljs.mv(path.resolve("./teamspeak3-server_*"), path.resolve(serverPath));
+    fs.chmodSync(path.resolve(serverPath, "./ts3server"), "744");
+    fs.writeFileSync(path.resolve(serverPath, "./.ts3server_license_accepted"));
+    shelljs.exec(
+      "cd " +
+        path.resolve(serverPath) +
+        " && ./ts3server license_accepted=1 serveradmin_password=" +
+        serveradmin_password +
+        " > /dev/null & echo $! > " +
+        path.resolve(serverPath, "./ts3server.pid"),
+      { async: true, silent: true }
+    );
+    await new Promise(resolve => setTimeout(resolve, 5000));
+  },
+  stopServer: async serverPath => {
+    await shelljs.exec(
+      "kill -KILL $(cat " + path.resolve(serverPath, "./ts3server.pid") + ")",
+      { async: true, silent: true }
+    );
+    shelljs.rm("-R", path.resolve(serverPath));
+  }
+};
